a02dfddcfea148faf0a62e465b133142832ec83c
Lavanya9493 <lavanyajonnalagadda123@gmail.com>
2021-11-12 08:01:22 +0530

    Override

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Override2
+{
+    
+    class Base
+    {
+        public virtual void Print()
+        {
+            Console.WriteLine("Base.Print()");
+        }
+    }
+    class Derived:Base
+    {
+        public override void Print()
+        {
+            Console.WriteLine("Derived.Print()");
+        }
+    }
+        class Program
+    {
+        static void Main(string[] args)
+        {
+            Derived d=new Derived();
+            d.Print();
+            Base b=new Derived();
+            b.Print();
+        }
+    }
+}
